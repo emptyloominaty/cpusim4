32a6cb400758a95c5ad89e621e8aeb8d80f9f8ea
emptyloominaty <cipuksiblol@gmail.com>
2025-06-19 12:46:59 +0200

    2

--- Cs/Cpu.cs
+++ Cs/Cpu.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Windows.Documents;
 
 namespace CpuSim4 {
     public class Cpu {
@@ -28,11 +29,39 @@ namespace CpuSim4 {
         public int[] registers;
         public float[] registersF;
 
+        public CacheLine[] cacheD;
+        public CacheLine[] cacheI;
+
+        public long timeClockA = 0;
+        public long timeClockB = 0;
+        public long timeClockC = 0;
+        public long timeClockD = 0;
+
+        public long timeStart;
+        public long timeEnd;
+        public long timeA;
+        public long timeB;
+        public long timeC;
+        public long timeD;
+
+        public long cyclesDone = 0;
+        public long cyclesDoneA = 0;
+        public long cyclesDoneB = 0;
+        public long instructionsDone = 0;
+
+        public int cyclesTotal = 0;
+        public int cyclesExecuting = 0;
+
+        public PipelineStage[] pipeline = new PipelineStage[3];
+
         public Cpu(OpCode[] opCodes) {
             this.opCodes = opCodes;
             Reset();
         }
         public void Reset() {
+            cacheI = new CacheLine[256];
+            cacheD = new CacheLine[256];
+
             registers = new int[38];
             for (int i = 0; i < registers.Length; i++) {
                 registers[i] = 0;
@@ -50,6 +79,146 @@ namespace CpuSim4 {
 
             interruptHw = false;
             halted = false;
+
+            timeClockA = DateTime.Now.Ticks * 100;
+            timeClockB = DateTime.Now.Ticks * 100;
+            timeClockC = 0;
+            timeClockD = 0;
+
+            cyclesDone = 0;
+            cyclesDoneA = 0;
+            cyclesDoneB = 0;
+            instructionsDone = 0;
+
+            cyclesTotal = 0;
+            cyclesExecuting = 0;
+            pipeline = new PipelineStage[3];
+            pipeline[0] = new PipelineStage { op = 22 };
+            pipeline[1] = new PipelineStage { op = 22 };
+            pipeline[2] = new PipelineStage { op = 22 };
         }
+
+        public void StartCpu() {
+            cpuThread = new Thread(new ThreadStart(Run));
+            cpuThread.IsBackground = true;
+            cpuThread.Start();
+        }
+
+        public void Run() {
+            
+            while (threadRunning) {
+                long timeClockWait = 1000000000 / clockSet;
+                while (cpuRunning) {
+                    if (maxClock) {
+                        Main();
+                    } else {
+                        timeClockA = DateTime.Now.Ticks * 100;
+                        Main();
+                        timeClockD = 0;
+                        while (timeClockD < timeClockWait) {
+                            timeClockB = DateTime.Now.Ticks * 100;
+                            timeClockD = timeClockB - timeClockA;
+                        }
+                    }
+                }
+                try {
+                    Thread.Sleep(100);
+                } catch (ThreadInterruptedException e) {
+                    throw new Exception("InterruptedException occurred", e);
+                }
+            }
+        }
+
+        public void Main() {
+            cyclesDoneA++;
+            timeA = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            if (timeA - timeB > 1000) {
+                clock = cyclesDoneA - cyclesDoneB;
+                timeB = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                cyclesDoneB = cyclesDoneA;
+                if (timeA - timeD > 5000) {
+                    cyclesTotal /= 2;
+                    cyclesExecuting /= 2;
+                    timeD = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                }
+            }
+            if (timeA - timeC > 33.3) {
+                timeC = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+                timeEnd = timeC;
+            }
+            cyclesTotal++;
+            if (halted) {
+                return;
+            }
+
+
+            cyclesExecuting++;
+            cyclesDone++;
+
+
+            Execute(pipeline[2]);
+            Decode(pipeline[1]);
+
+            pipeline[2] = pipeline[1];
+            pipeline[1] = pipeline[0];
+            pipeline[0] = Fetch();
+        }
+
+
+        public PipelineStage Fetch() {
+            int pc = registers[33];
+
+
+            PipelineStage ps = new PipelineStage { };
+
+            return ps;
+        }
+
+
+        public void Decode(PipelineStage ps) {
+            if (ps.op == 22) {
+                return;
+            }
+        }
+
+        public void Execute(PipelineStage ps) {
+            if (ps.op == 22) {
+                return;
+            }
+        }
+
+
+
+        public void StopCpu() {
+            cpuRunning = false;
+        }
+
     }
+
+    public struct PipelineStage {
+        public byte op;
+        public byte arg1;
+        public byte arg2;
+        public byte arg3;
+        public byte arg4;
+        public byte arg5;
+        public byte arg6;
+
+        public int address3; //address +3
+        public int address2; //address +2
+        public int address1; //address +1
+        public int address0; //address +0
+
+        public byte cyclesToExecute; //0=1
+        public byte cyclesExecuted;
+    }
+
+    public struct CacheLine {
+        public bool valid;
+        public int tag;
+        public byte[] data;
+    }
+
 }
+
+
---
+++ Cs/Memory.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpuSim4 {
+    public static class Memory {
+        public static byte[] Data = new byte[16777216];
+        public static bool[] DataCanWriteArray = new bool[16777216];
+
+        static Memory() {
+            Init();
+        }
+
+        public static void Init() {
+            Data = new byte[16777216];
+            DataCanWriteArray = new bool[16777216];
+            for (int i = 0; i < Data.Length; i++) {
+                Data[i] = 0;
+                DataCanWriteArray[i] = true;
+            }
+
+            for (int i = 0x400000; i <= 0xffffff; i++) {
+                DataCanWriteArray[i] = false;
+            }
+
+            //TEST CODE
+            /*Write(7340032, 3, true);
+            Write(7340033, 0, true);
+            Write(7340034, 0x70, true);
+            Write(7340035, 0, true);
+            Write(7340036, 0, true);
+
+            Write(7340037, 3, true);
+            Write(7340038, 1, true);
+            Write(7340039, 0x70, true);
+            Write(7340040, 0, true);
+            Write(7340041, 2, true);
+
+            Write(7340042, 1, true);
+            Write(7340043, 0, true);
+            Write(7340044, 1, true);
+            Write(7340045, 2, true);
+
+            Write(7340046, 15, true);
+            Write(7340047, 3, true);
+
+            Write(7340048, 14, true);
+            Write(7340049, 4, true);
+            Write(7340050, 0, true);
+            Write(7340051, 1, true);
+            Write(7340052, 255, true);
+            Write(7340053, 255, true);
+
+            Write(7340054, 26, true);
+            Write(7340055, 4, true);
+            Write(7340056, 3, true);
+            Write(7340057, 0x70, true);
+            Write(7340058, 0, true);
+            Write(7340059, 0, true);
+
+            Write(7340060, 24, true);
+            Write(7340061, 0x70, true);
+            Write(7340062, 0x00, true);
+            Write(7340063, 0x2F, true);
+
+            Write(7340064, 0, true);
+
+            Write(7340065, 13, true);
+            Write(7340066, 0, true);
+            Write(7340067, 25, true);
+            Write(7340068, 255, true);
+            Write(7340069, 255, true);
+
+            Write(7340070, 15, true);
+            Write(7340071, 1, true);
+
+            Write(7340072, 26, true);
+            Write(7340073, 0, true);
+            Write(7340074, 1, true);
+            Write(7340075, 0x70, true);
+            Write(7340076, 0x00, true);
+            Write(7340077, 0x26, true);
+
+            Write(7340078, 25, true);
+
+            Write(7340079, 23, true);
+            Write(7340080, 0x70, true);
+            Write(7340081, 0x00, true);
+            Write(7340082, 0x21, true);*/
+        }
+
+        public static byte Read(int address) {
+            if (address < 0 || address >= Data.Length) {
+                return 0;
+            }
+            return Data[address];
+        }
+        public static void Write(int address, byte data, bool forceWrite = false) {
+            if (address >= 0 && address < Data.Length) {
+                if (DataCanWriteArray[address] || forceWrite) {
+                    Data[address] = data;
+                }
+            }
+        }
+
+    }
+}
